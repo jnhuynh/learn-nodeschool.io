e6ac2f7375af64fd4c04052621360d273c231886
John Huynh <john@huynh.io>
2014-05-08 18:59:29 -0400

    Rewrote write-collect to use bl.

--- learnyounode/http-collect/get-url.js
+++ learnyounode/http-collect/get-url.js
@@ -1,23 +1,37 @@
 var http = require("http");
+var bl   = require("bl");
 
 function getUrl(url, callback){
-  var dataArray = [];
-
+  // var dataArray = [];
+  //
+  // return http.get(url, function(res) {
+  //   res.setEncoding("utf-8");
+  //
+  //   res.on("error", function(error) {
+  //     console.log("Error: ", error);
+  //   });
+  //
+  //   res.on("data", function(data) {
+  //     dataArray.push(data);
+  //   });
+  //
+  //   res.on("end", function() {
+  //     callback(dataArray);
+  //   });
+  // });
   return http.get(url, function(res) {
+    var blDataHandler = bl(function(err, data){
+      if (err) {
+        console.log("Error: ", err);
+        return;
+      }
+
+      callback(data);
+    });
+
     res.setEncoding("utf-8");
-
-    res.on("error", function(error) {
-      console.log("Error: ", error);
-    });
-
-    res.on("data", function(data) {
-      dataArray.push(data);
-    });
-
-    res.on("end", function() {
-      callback(dataArray);
-    });
-  });
+    res.pipe(blDataHandler);
+  })
 }
 
 module.exports = getUrl;
--- learnyounode/http-collect/main.js
+++ learnyounode/http-collect/main.js
@@ -3,9 +3,11 @@ var getUrl = require("./get-url.js");
 var url = process.argv[2];
 
 getUrl(url, function(data) {
-  var dataString = data.reduce(function(prev, cur) {
-    return prev += cur.toString();
-  }, "");
+  // var dataString = data.reduce(function(prev, cur) {
+  //   return prev += cur.toString();
+  // }, "");
+
+  var dataString = data.toString();
 
   console.log(dataString.length);
   console.log(dataString);
